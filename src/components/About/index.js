4f80cde6eeaddecfcfcf7c1e69833ccd014533d1
Fathan Mubarok <falonez01@gmail.com>
2023-01-31 22:26:13 +0700

    Add Animation Effect About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,13 +3,20 @@ import "./about.css";
 import { Profile } from "../../assets";
 
 const About = () => {
+  window.addEventListener("scroll", function () {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset >= 300) {
+      document.getElementById("about-image").classList.add("active-about");
+      document.getElementById("about-text").classList.add("active-about");
+    }
+  });
   return (
     <>
       <div className="about" id="about">
-        <div className="about-image">
+        <div className="about-image" id="about-image">
           <img src={Profile} alt=""></img>
         </div>
-        <div className="about-text">
+        <div className="about-text" id="about-text">
           <h1 className="about-title">About Me</h1>
           <p className="about-subtitle">
             I am a 6th semester student at STMIK Mardira Indonesia, with a
